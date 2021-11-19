70e6149b3f74d40b7d45afb5cc17dca9e997a6cd
Anton <71210712@students.ukdw.ac.id>
2021-11-19 12:35:42 +0700

    UG10_B_71210712

---
+++ 1_B_71210712.py
@@ -0,0 +1,8 @@
+print ("Masukan nilai huruf PrakTekKom anda : A")
+print ("Rentang nilai PrakTekKom anda adalah 85 - 100")
+
+print ("Masukan nilai huruf PrakTekKom anda : B-")
+print ("Rentang nilai PrakTekKom anda adalah 65 - 69")
+
+print ("Masukkan nilai huruf PrakTekKom anda C++")
+print ("Inputan anda salah atau nilai huruf tidak ada pada standar nilai")
